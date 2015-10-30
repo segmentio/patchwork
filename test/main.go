cbe783091fa1d0466b358ebc85b3e918e90a0e7e
Prateek Srivastava <f2prateek@gmail.com>
2015-10-29 23:27:49 -0700

    add test repo

---
+++ test/main.go
@@ -0,0 +1,46 @@
+// Script to update the Circle CI configuration for analytics.js-integrations
+// to use Saucelabs in tests.
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/segmentio/patchwork"
+)
+
+func main() {
+	p := patchwork.New(os.Getenv("GITHUB_TOKEN"), os.Getenv("CIRCLE_TOKEN"))
+
+	n := strconv.Itoa(rand.Intn(4000))
+
+	opts := &patchwork.ApplyOptions{}
+	opts.Message = "Testing Patchwork!" + n
+	opts.Branch = "test" + n
+	opts.Repos = []patchwork.Repository{
+		{"segmentio", "patchwork-test"},
+	}
+
+	p.Apply(*opts, func(repo *github.Repository, dir string) {
+		// sed wasn't playing nicely :(
+		circleFile := dir + "/circle.yml"
+		circleConfig, err := ioutil.ReadFile(circleFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lines := strings.Split(string(circleConfig), "\n")
+		for i, line := range lines {
+			lines[i] = strings.Replace(line, "testing", "testing success", 1)
+		}
+		output := strings.Join(lines, "\n")
+		err = ioutil.WriteFile(circleFile, []byte(output), 0644)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
+}
